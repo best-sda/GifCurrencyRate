cbf6b318a2da948102a32df5e882b380c9ebca22
best-sda <best-sda@mail.ru>
2021-01-14 22:11:46 +0300

    Initial commit

---
+++ src/main/java/com/example/gifcurrencyrate/GifcurrencyrateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gifcurrencyrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifcurrencyrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifcurrencyrateApplication.class, args);
+	}
+
+}
